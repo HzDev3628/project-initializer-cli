43467245da4ddd3529f7545f3ba030be88cc712a
Artem Kharchuk <artem.khar5uk@gmail.com>
2025-08-21 13:17:43 +0300

    fix: about author command

--- src/commands/about.ts
+++ src/commands/about.ts
@@ -4,8 +4,7 @@ import { log } from '@/lib/utils'
 export function aboutAuthor() {
   return log(
     `
-${chalk.bold('Full Name:')} Artem Kharhcuk
-${chalk.bold('Nickname:')} HzDev
+${chalk.bold('Name:')} Artem Kharhcuk
 ${chalk.bold('Email:')} artem.khar5uk@gmail.com
 ${chalk.bold('Social Media Links:')} https://artem-khar.vercel.app/links
 `,
